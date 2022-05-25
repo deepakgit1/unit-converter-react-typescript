86fb3c1e2319d6ee9078694ac62aae40b3e38c6e
deepakgit1 <deepakgaidhane9@gmail.com>
2022-05-25 17:34:31 +0530

    make ui better for main screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,10 @@ const Header = (props: Props) => {
   return (
     <div>
       <>
-        <Navbar bg="info" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home" className='m-auto h1'>
-               Unit Converter
+        <Navbar bg="dark" variant='dark'>
+          <Container style={{boxShadow:"0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"}}>
+            <Navbar.Brand href="#home" className='m-auto h1' style={{fontFamily:"cursive"}}>
+              Welcome to Unit Converter
             </Navbar.Brand>
           </Container>
         </Navbar>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 import React, { FormEvent, useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
+import "../components/css/home.css"
+import {SiConvertio} from "react-icons/si"
 
 type Props = {}
 
@@ -62,13 +64,21 @@ const Home = (props: Props) => {
                     break;
             }
         }
-
     }
 
 
     return (
         <div>
-            <Container className='w-50 mt-2'>
+            <Container className='w-50 mt-2' 
+            style={{background:"linear-gradient(to right, #36d1dc, #5b86e5)",
+            padding:"50px",
+            borderRadius:"10px",
+            fontFamily:"cursive",
+            boxShadow:"rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"
+            }}>
+                <div style={{textAlign:"center" ,marginTop:"-30px"}}>
+                <SiConvertio className='button2' style={{fontSize:"60px"}}/>
+                </div>
                 <Form.Label className='mt-2 h3' size="lg">From:</Form.Label>
                 <Form.Select size="lg" onChange={(e) => setInputUnit(e.target.value)}>
                     <option>Select unit to convert</option>
@@ -79,8 +89,8 @@ const Home = (props: Props) => {
                     <option value="TONNE">Tonne</option>
                 </Form.Select>
 
-                <Form.Label className='mt-2 h5 text-info' size="lg">Enter Value</Form.Label>
-                <Form.Control type="number" placeholder='Enter Value' onChange={(e) => setVal(e.target.value)} />
+                <Form.Label className='mt-2 h5 text-light' size="lg">Enter Value</Form.Label>
+                <Form.Control type="number" placeholder='Enter Value' onChange={(e) => setVal(e.target.value)}  />
 
                 <Form.Label className='mt-2 h3' size="lg">To:</Form.Label>
                 <Form.Select size="lg" onChange={(e) => setOutputUnit(e.target.value)}>
@@ -91,11 +101,11 @@ const Home = (props: Props) => {
                     <option value="POUND">Pound</option>
                     <option value="TONNE">Tonne</option>
                 </Form.Select>
-                <Form.Label className='mt-2 h5 text-info' defaultValue={result} size="lg">Result</Form.Label>
+                <Form.Label className='mt-2 h5 text-light' defaultValue={result} size="lg">Result</Form.Label>
 
                 <Form.Control type="number" defaultValue={result} placeholder='Result' />
-                <div style={{ alignItems: "center", margin: "auto" }}>
-                    <Button className='bg-success mt-3' size='lg' onClick={handleClick}>Convert</Button>
+                <div className='d-grid gap-2 mt-3'>
+                    <Button className='button' style={{borderRadius:"10px"}} size='lg' onClick={handleClick}>Convert</Button>
                 </div>
             </Container>
         </div>
